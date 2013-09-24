63651a97706c72b2d8028743514b3dc08577e073
dadhi <max.dadhi@gmail.com>
2013-09-24 18:25:22 +0300

    added: IGetChanged, IChanged, Changed and ChangedException.
    todo: Change.Value getter with notification.

--- FunTools/Changed/Changed.cs
+++ FunTools/Changed/Changed.cs
@@ -1,29 +1,72 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace FunTools.Changed
 {
-    public abstract class Changed<TValue> : INotifyPropertyChanged
+    public interface IGetChanged<TValue> : INotifyPropertyChanged
     {
-        public abstract TValue Value { get; }
+        TValue Value { get; }
+    }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+    public interface IChanged<TValue> : IGetChanged<TValue>
+    {
+        new TValue Value { get; set; }
+    }
 
+    public sealed class Changed<TValue> : IChanged<TValue>
+    {
         public static implicit operator TValue(Changed<TValue> changed)
         {
             return changed.Value;
         }
 
-        public static readonly string VALUE_AS_PROPERTY_NAME = "Value";
+        public static readonly PropertyChangedEventArgs ChangedEventArgs = new PropertyChangedEventArgs("Value");
 
-        #region Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void NotifyChanged()
+        public delegate bool ChangeCondition(TValue oldValue, TValue setValue);
+
+        public Changed(
+            TValue initialValue = default(TValue),
+            ChangeCondition changeCondition = null) // Use !Equals(a, b) by default.
         {
-            var handler = PropertyChanged;
-            if (handler != null)
-                handler(this, new PropertyChangedEventArgs(VALUE_AS_PROPERTY_NAME));
+            _value = initialValue;
+            _changeCondition = changeCondition;
         }
 
-        #endregion
+        public TValue Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if (_changeCondition != null && !_changeCondition(_value, value) || Equals(_value, value))
+                    return;
+
+                if (_changing)
+                    throw new ChangedException(string.Format("Recursive change dectected of {0}.", _value));
+
+                _value = value;
+
+                var changed = PropertyChanged;
+                if (changed == null) 
+                    return;
+
+                _changing = true;
+                try { changed(this, ChangedEventArgs); }
+                finally { _changing = false; }
+            }
+        }
+
+        private TValue _value;
+        private readonly ChangeCondition _changeCondition;
+        private bool _changing;
+    }
+
+    public class ChangedException : InvalidOperationException
+    {
+        public ChangedException(string message) : base(message) { }
     }
 }
