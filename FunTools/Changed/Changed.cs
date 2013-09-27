006d2d5bedc2b43f43e2726ab097d92b1a562224
dadhi <max.dadhi@gmail.com>
2013-09-28 02:41:27 +0300

    added: GetComputed - implemented as lazy and performance wise.

--- FunTools/Changed/Changed.cs
+++ FunTools/Changed/Changed.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace FunTools.Changed
 {
-    public interface IGetChanged<TValue> : INotifyPropertyChanged
+    using IGetChanged = INotifyPropertyChanged;
+
+    public interface IGetChanged<TValue> : IGetChanged
     {
         TValue Value { get; }
     }
@@ -13,16 +18,47 @@ namespace FunTools.Changed
         new TValue Value { get; set; }
     }
 
+    public static class Changed
+    {
+        public static Changed<TValue> From<TValue>(TValue initialValue = default(TValue), Changed<TValue>.ChangeCondition isChanged = null)
+        {
+            return new Changed<TValue>(initialValue, isChanged);
+        }
+
+        public static void PushAction(Action<IGetChanged> action)
+        {
+            _stack = new Stack<Action<IGetChanged>>(action, _stack);
+        }
+
+        public static void PopAction()
+        {
+            _stack = _stack.Tail;
+        }
+
+        #region Implementation
+
+        [ThreadStatic] private static Stack<Action<IGetChanged>> _stack;
+
+        internal sealed class Stack<T>
+        {
+            public readonly T Head;
+            public readonly Stack<T> Tail;
+
+            public Stack(T head, Stack<T> tail = null)
+            {
+                Head = head;
+                Tail = tail;
+            }
+        }
+
+        #endregion
+    }
+
     public sealed class Changed<TValue> : IChanged<TValue>
     {
         public static implicit operator TValue(Changed<TValue> changed)
         {
             return changed.Value;
-        }
-
-        public static implicit operator Changed<TValue>(TValue initialValue)
-        {
-            return new Changed<TValue>(initialValue);
         }
 
         public static readonly PropertyChangedEventArgs ChangedEventArgs = new PropertyChangedEventArgs("Value");
@@ -80,19 +116,109 @@ namespace FunTools.Changed
         #endregion
     }
 
-    public sealed class GetComputed<TValue> : IGetChanged<TValue>
+    public sealed class GetComputed<TValue> : IGetChanged<TValue>, IDisposable
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        
-        public TValue Value { get; private set; }
-    }
-
-    public static class Changed
-    {
-        public static Changed<TValue> From<TValue>(TValue initialValue = default(TValue), Changed<TValue>.ChangeCondition isChanged = null)
+        public GetComputed(Func<TValue> compute)
         {
-            return new Changed<TValue>(initialValue, isChanged);
+            _compute = compute.ThrowIfNull();
+            _observed = new List<Observed>();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TValue Value
+        {
+            get
+            {
+                SetAllAsUnchanged();
+                Changed.PushAction(ObserveChanged);
+                TValue value;
+                try
+                {
+                    value = _compute();
+                }
+                finally
+                {
+                    Changed.PopAction();
+                }
+                RemoveUnchanged();
+                return value;
+            }
+        }
+
+        public void Dispose()
+        {
+            for (var i = 0; i < _observed.Count; i++)
+            {
+                _observed[i].Changed.PropertyChanged -= NotifyChanged;
+                _observed[i].Changed = null;
+            }
+        }
+
+        #region Implementation
+
+        private readonly Func<TValue> _compute;
+        private readonly List<Observed> _observed;
+
+        private void NotifyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var changedEvent = PropertyChanged;
+            if (changedEvent != null)
+                changedEvent(this, e);
+        }
+
+        private void SetAllAsUnchanged()
+        {
+            for (var i = 0; i < _observed.Count; i++)
+                _observed[i].ChangedRecently = false;
+        }
+
+        private void ObserveChanged(IGetChanged changed)
+        {
+            Observed spareObserved = null;
+            for (var i = 0; i < _observed.Count; ++i)
+            {
+                var observed = _observed[i];
+                if (observed.Changed == null)
+                    spareObserved = observed;
+                else if (observed.Changed == changed)
+                {
+                    observed.ChangedRecently = true;
+                    return;
+                }
+            }
+
+            if (spareObserved == null)
+                _observed.Add(new Observed {Changed = changed, ChangedRecently = true});
+            else
+            {
+                spareObserved.Changed = changed;
+                spareObserved.ChangedRecently = true;
+            }
+
+            changed.PropertyChanged += NotifyChanged;
+        }
+
+        private void RemoveUnchanged()
+        {
+            for (var i = 0; i < _observed.Count; i++)
+            {
+                var observed = _observed[i];
+                if (!observed.ChangedRecently)
+                {
+                    observed.Changed.PropertyChanged -= NotifyChanged;
+                    observed.Changed = null;
+                }
+            }
+        }
+
+        private sealed class Observed
+        {
+            public IGetChanged Changed;
+            public bool ChangedRecently;
+        }
+
+        #endregion
     }
 
     public class ChangedException : InvalidOperationException
