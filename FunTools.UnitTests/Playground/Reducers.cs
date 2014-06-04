1bc33fd68d6617e2eb65a1ba6ea9ae3a18fcd2ac
dadhi <max.volkov@gmail.com>
2014-06-04 08:34:45 +0300

    added: Playground/Reducers.cs.

---
+++ FunTools.UnitTests/Playground/Reducers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunTools.UnitTests.Playground
+{
+    public static class Reducers
+    {
+        public delegate Func<IEnumerable<T>, R> Reduce<T, R>(R seed, Func<R, T, R> reduce);
+
+        public static Func<Reduce<X, R>, Reduce<X, R>> Mapping<X, Y, R>(Func<X, Y> map)
+        {
+            return reduce => (seed, func) => xs =>
+            {
+                foreach (var x in xs)
+                {
+                    
+                }
+            };
+        }
+    }
+
+}
