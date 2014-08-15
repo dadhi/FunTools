c5e91fcbfa6a75713271235d0514bcb0f8130b74
dadhi <max.volkov@gmail.com>
2014-08-15 11:45:16 +0300

    fixed: Running ball from other folder.

--- BuildScripts/Ball.bat
+++ BuildScripts/Ball.bat
@@ -5,6 +5,10 @@ echo:
 echo:Clean, build, pack nuget packages..
 echo:-----------------------------------
 
+set ScriptsDir="%~dp0"
+echo:Switching to %ScriptsDir%
+cd /d %ScriptsDir%  
+
 call Clean -nopause
 call :Check
 
@@ -19,14 +23,14 @@ rem call :Check
 
 echo:------------
 echo:All Success.
-echo:
 
 if not "%1"=="-nopause" pause 
 goto:eof
 
 :Check
 if ERRORLEVEL 1 (
-echo:Failed with ERRORLEVEL==%ERRORLEVEL%!
-if not "%1"=="-nopause" pause
+echo:Step failed with ERRORLEVEL==%ERRORLEVEL%!
 exit
-) else exit /b
+) else ( 
+exit /b
+)
